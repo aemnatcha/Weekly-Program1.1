34e81616d459f4e5bf409d95d03a1c422d934545
Natcha Bunsahwat <89073694+aemnatcha@users.noreply.github.com>
2021-08-18 01:21:15 +0700

    Update project1.c

--- Project1/project1.c
+++ Project1/project1.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 int main()
 {
-	int num1, num2, sum
-	printf("please insert Number1:");
-	scanf("%d",&num1);
-	printf("please insert Number2:");
-	scanf("%d",&num2);
-	sum = num1 + num2;
-	printf("sum of %d+%d=%d", num1, num2, sum);
-	getch();
-	return 0;
+  int n;
+   
+  printf("Input an integer\n");
+  scanf("%d", &n);
+
+  if (n & 1 == 1)
+    printf("Odd\n");
+  else
+    printf("Even\n");
+   
+  return 0;
 }
